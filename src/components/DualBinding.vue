5c11a58a0b1dcc11e9873770e130336ea0afa9ff
Deividi-Lavitz <deividi.luvison@gmail.com>
2023-09-23 14:50:11 -0300

    2 files modified, 1 added, 2 deleted

---
+++ src/components/DualBinding.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+function inputHandle(event: Event) {
+  const textBox = event.currentTarget as HTMLInputElement;
+  console.log(textBox.valeu);
+}
+</script>
+
+<template>
+  <div>
+    <input type="text" placeholde="Type your name" @input="inputHandle" />
+    <span></span>
+  </div>
+</template>
+
+<style scoped></style>
