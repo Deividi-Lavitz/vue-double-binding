df806b8a8bea3357c3147026b04639323fdea3f5
Deividi-Lavitz <deividi.luvison@gmail.com>
2023-09-24 16:28:42 -0300

    1 file modified, 1 added

--- src/App.vue
+++ src/App.vue
@@ -2,10 +2,12 @@
 import DualBinding from './components/DualBinding.vue';
 import DualBindingV2 from './components/DualBindingV2.vue';
 import Conditions from './components/Conditions.vue';
+import Computed from './components/Computed.vue';
 </script>
 
 <template>
   <Conditions />
+  <Computed />
 </template>
 
 <style scoped></style>
---
+++ src/components/Computed.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+import { ref, computed } from 'vue';
+
+const autor = ref({
+  id: 1,
+  name: 'Lavitz',
+  books: [],
+});
+
+const message = computed(() => {
+  return autor.value.books.lenght > 0 ? 'Lots of Books' : 'No Books';
+});
+</script>
+
+<template>
+  <h1>{{ autor.name }}</h1>
+  <h2>Books:</h2>
+  <span>{{ message }}</span>
+</template>
+
+<style scoped></style>
