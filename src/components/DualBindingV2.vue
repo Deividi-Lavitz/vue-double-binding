c7cc62aa7ba404bb0de660047c4ee9f33a965f02
Deividi-Lavitz <deividi.luvison@gmail.com>
2023-09-24 14:37:12 -0300

    1 file modified

--- src/components/DualBindingV2.vue
+++ src/components/DualBindingV2.vue
@@ -2,12 +2,40 @@
 import { ref } from 'vue';
 
 const textValue = ref('');
+const checkedState = ref(true);
+const multiChecked = ref([]);
+const selection = ref('');
+//const selection = ref([]);
 </script>
 
 <template>
   <div>
-    <input type="text" placeholder="Type your name" v-model="textValue" />
+    <input type="text" placeholder="Type your name" v-model.trim="textValue" />
     <span>{{ textValue }}</span>
+  </div>
+  <div>
+    <input type="checkbox" v-model="checkedState" id="checkbox" />
+    <label for="checkbox">{{ checkedState }}</label>
+  </div>
+  <div>
+    <input
+      type="checkbox"
+      Value="Warrior"
+      v-model="multiChecked"
+      id="warrior"
+    />
+    <label for="warrior">Warrior</label>
+    <input type="checkbox" Value="Mage" v-model="multiChecked" id="mage" />
+    <label for="mage">Mage</label>
+  </div>
+  <div>{{ multiChecked }}</div>
+  <div>
+    <select v-model="selection">
+      <option value="" disabled>Choose Wisely!</option>
+      <option>A</option>
+      <option>B</option>
+      <option>C</option>
+    </select>
   </div>
 </template>
 
